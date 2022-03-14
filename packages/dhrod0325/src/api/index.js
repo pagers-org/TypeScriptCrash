d6534923a216ac3f622280e5e2613592232bcc97
설아아빠 <dhrod0325@naver.com>
2022-03-14 22:04:16 +0900

    request 함수 분리중

--- packages/dhrod0325/src/api/index.js
+++ packages/dhrod0325/src/api/index.js
@@ -1,54 +1,52 @@
-export async function login(url, data) {
-  const config = {
+async function request(url, data, config = {
     method: 'POST',
-    headers: new Headers({ 'content-type': 'application/json' }),
-  };
-  if (data) config.body = JSON.stringify(data);
-  const response = await fetch(url, config);
-  const parse = await response.json();
-  return parse;
+    headers: new Headers({'content-type': 'application/json'}),
+}) {
+    if (data) {
+        config.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, config);
+
+    return await response.json();
+}
+
+export async function login(data) {
+    return request('http://localhost:3000/api/user/login', data);
 }
 
 export async function signup(url, data) {
-  const config = {
-    method: 'POST',
-    headers: new Headers({ 'content-type': 'application/json' }),
-  };
-  if (data) config.body = JSON.stringify(data);
-  const response = await fetch(url, config);
-  const parse = await response.json();
-  return parse;
+    return request('http://localhost:3000/api/user', data);
 }
 
 export async function getBookmarkList(url, data) {
-  const config = {
-    method: 'POST',
-    headers: new Headers({ 'content-type': 'application/json' }),
-  };
-  if (data) config.body = JSON.stringify(data);
-  const response = await fetch(url, config);
-  const parse = await response.json();
-  return parse;
+    const config = {
+        method: 'POST',
+        headers: new Headers({'content-type': 'application/json'}),
+    };
+    if (data) config.body = JSON.stringify(data);
+    const response = await fetch(url, config);
+    const parse = await response.json();
+    return parse;
 }
 
 export async function addBookmark(url, data) {
-  const config = {
-    method: 'POST',
-    headers: new Headers({ 'content-type': 'application/json' }),
-  };
-  if (data) config.body = JSON.stringify(data);
-  const response = await fetch(url, config);
-  const parse = await response.json();
-  return parse;
+    const config = {
+        method: 'POST',
+        headers: new Headers({'content-type': 'application/json'}),
+    };
+    if (data) config.body = JSON.stringify(data);
+    const response = await fetch(url, config);
+    return await response.json();
 }
 
 export async function removeBookmark(url, data) {
-  const config = {
-    method: 'DELETE',
-    headers: new Headers({ 'content-type': 'application/json' }),
-  };
-  if (data) config.body = JSON.stringify(data);
-  const response = await fetch(url, config);
-  const parse = await response.json();
-  return parse;
+    const config = {
+        method: 'DELETE',
+        headers: new Headers({'content-type': 'application/json'}),
+    };
+    if (data) config.body = JSON.stringify(data);
+
+    const response = await fetch(url, config);
+    return await response.json();
 }
