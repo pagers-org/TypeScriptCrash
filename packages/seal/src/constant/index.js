a7cd618efabfa7cccdc155cc45732a3fcda2d29c
parksil0 <sirisiri0809@gmail.com>
2022-03-15 05:15:55 +0900

    feat : 메세지 관련 상수화

--- packages/seal/src/constant/index.js
+++ packages/seal/src/constant/index.js
@@ -5,3 +5,12 @@ export const INDEX_PAGE = 'http://localhost:5510';
 export const LOCAL_STORAGE_KEY = {
   USER_TOKEN: 'user_token',
 };
+
+export const ERROR_MESSAGE = {
+  WRONG_PASSWORD: '패스워드를 확인해주세요.',
+  INVALID_EMAIL_FORMAT: '옳지 않은 이메일 형식입니다.',
+};
+
+export const SUCCESS_MESSAGE = {
+  SIGNUP: '회원가입이 완료되었습니다.\n로그인해주세요.',
+};
