4672a651899eaaf1c7f73d5ec35e4b16a020b307
goflvhxj2547 <goflvhxj2547@gmail.com>
2022-03-16 00:52:40 +0900

    ✒️Feat: 매직 넘버를 상수화한다.

--- packages/parang/src/constants/index.js
+++ packages/parang/src/constants/index.js
@@ -1,1 +1,19 @@
+// 브라우저 요청
 export const BASE_URL = 'http://localhost:3000';
+export const IMAGE_API_URL = 'https://randomfox.ca/images';
+export const HTTP_METHODS = {
+  GET: 'GET',
+  POST: 'POST',
+  PATCH: 'PATCH',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+};
+
+// 정규표현식
+export const REGEXP_EMAIL =
+  /^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+// 기타
+export const GET_IMAGE_COUNT = 10;
+
+export const USER_TOKEN = 'USER_TOKEN';
