8aaf66da1ebfb231d07a5ae25e4269b7f97a93e8
sunhwa <2017sunhwa@gmail.com>
2022-03-15 09:32:38 +0900

    🗃Refactor: API 중복 코드 제거 공통 함수 추가

--- packages/console/src/api/index.js
+++ packages/console/src/api/index.js
@@ -1,54 +1,30 @@
-export async function login(url, data) {
+const fetchFn = async (url, data, method) => {
   const config = {
-    method: 'POST',
+    method: method,
     headers: new Headers({ 'content-type': 'application/json' }),
   };
   if (data) config.body = JSON.stringify(data);
   const response = await fetch(url, config);
   const parse = await response.json();
   return parse;
+};
+
+export async function login(url, data) {
+  return fetchFn(url, data, 'POST');
 }
 
 export async function signup(url, data) {
-  const config = {
-    method: 'POST',
-    headers: new Headers({ 'content-type': 'application/json' }),
-  };
-  if (data) config.body = JSON.stringify(data);
-  const response = await fetch(url, config);
-  const parse = await response.json();
-  return parse;
+  return fetchFn(url, data, 'POST');
 }
 
 export async function getBookmarkList(url, data) {
-  const config = {
-    method: 'POST',
-    headers: new Headers({ 'content-type': 'application/json' }),
-  };
-  if (data) config.body = JSON.stringify(data);
-  const response = await fetch(url, config);
-  const parse = await response.json();
-  return parse;
+  return fetchFn(url, data, 'POST');
 }
 
 export async function addBookmark(url, data) {
-  const config = {
-    method: 'POST',
-    headers: new Headers({ 'content-type': 'application/json' }),
-  };
-  if (data) config.body = JSON.stringify(data);
-  const response = await fetch(url, config);
-  const parse = await response.json();
-  return parse;
+  return fetchFn(url, data, 'POST');
 }
 
 export async function removeBookmark(url, data) {
-  const config = {
-    method: 'DELETE',
-    headers: new Headers({ 'content-type': 'application/json' }),
-  };
-  if (data) config.body = JSON.stringify(data);
-  const response = await fetch(url, config);
-  const parse = await response.json();
-  return parse;
+  return fetchFn(url, data, 'DELETE');
 }
