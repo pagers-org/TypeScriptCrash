fcd94a8963820290ad4f811ee83d001415085423
yechu <yechukim217@gmail.com>
2022-03-16 22:03:29 +0900

    refactor: 로그인/회원가입 페이지

--- packages/yechu/src/login.js
+++ packages/yechu/src/login.js
@@ -1,49 +1,59 @@
 import '../assets/page/login.css';
 import { login, signup } from './api/index.js';
-import { $, $all } from './helper/index.js';
-import { REG_EMAIL } from './constant';
+import { $all } from './helper/index.js';
+import { REG_EMAIL, LOGIN } from './constant';
 import { setUserInfo } from './storage';
 
+function toggleForm() {
+  $all('.forms').forEach(form => {
+    form.classList.toggle('hidden');
+  });
+}
+
 $all('.message a').forEach(tag => {
-  tag.addEventListener('click', () => {
-    $all('.forms').forEach(form => {
-      form.classList.toggle('hidden');
+  tag.addEventListener('click', toggleForm);
+});
+
+$all('button').forEach(btn => {
+  btn.addEventListener('click', async e => {
+    e.preventDefault();
+
+    const { parentElement, innerText: targetButton } = e.target;
+    const email = parentElement.children[0].value;
+    const password = parentElement.children[1].value;
+
+    if (targetButton === LOGIN) {
+      const data = await login('/user/login', {
+        email,
+        password,
+      });
+
+      if (!data[0]) alert('올바르지 않은 인증정보입니다. ');
+      const { _id, email: userEmail } = data[0];
+      alert(`환영합니다, ${userEmail}님!`);
+      setUserInfo(_id);
+      goMainPage();
+      return;
+    }
+
+    const passwordConfirm = parentElement.children[2].value;
+    if (password !== passwordConfirm) return alert('패스워드를 확인해주세요.');
+    if (!REG_EMAIL.test(email)) return alert('옳지 않은 이메일 형식입니다.');
+
+    await signup('/user', {
+      email,
+      password,
+      status: 0,
     });
+
+    alert('회원가입이 완료되었습니다.\n로그인해주세요.');
+    goLoginPage();
   });
 });
 
-$('button[data-submit="signup"]').addEventListener('click', async event => {
-  event.preventDefault();
-
-  const email = $('#signup-email').value;
-  const password = $('#signup-password').value;
-  const passwordConfirm = $('#signup-password-confirm').value;
-
-  if (password !== passwordConfirm) return alert('패스워드를 확인해주세요.');
-
-  if (!REG_EMAIL.test(email)) return alert('옳지 않은 이메일 형식입니다.');
-
-  await signup('/user', {
-    email,
-    password,
-    status: 0,
-  });
-
-  alert('회원가입이 완료되었습니다.\n로그인해주세요.');
-});
-
-$('button[data-submit="login"]').addEventListener('click', async event => {
-  event.preventDefault();
-
-  const email = $('#login-email').value;
-  const password = $('#login-password').value;
-
-  const data = await login('/user/login', {
-    email,
-    password,
-  });
-  const { _id, email: userEmail } = data[0];
-  alert(`환영합니다, ${userEmail}님!`);
-  setUserInfo(_id);
+const goMainPage = () => {
   location.replace('http://localhost:5510/');
-});
+};
+const goLoginPage = () => {
+  toggleForm();
+};
