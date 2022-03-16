8cbb27fd58491187b18df9d3c3a50cb36e9f45d0
yechu <yechukim217@gmail.com>
2022-03-16 19:28:49 +0900

    refactor: parseData-> requestData

--- packages/yechu/src/api/common.js
+++ packages/yechu/src/api/common.js
@@ -1,15 +1,16 @@
 const BASE_URL = 'http://localhost:3000/api';
 
-export async function parseData(resource, data) {
-
-  const response = await fetch(BASE_URL + resource, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: data && JSON.stringify(data),
-  });
-
-  console.log(response, resource, data)
-  return response.json();
-};
+export async function requestData(resource, data) {
+  try {
+    const response = await fetch(BASE_URL + resource, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data && JSON.stringify(data),
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error.message);
+  }
+}
---
+++ packages/yechu/src/api/index.js
@@ -0,0 +1,21 @@
+import { requestData } from './common';
+
+export async function getBookmarkList(resource, data) {
+  return await requestData(resource, data);
+}
+
+export async function addBookmark(resource, data) {
+  return await requestData(resource, data);
+}
+
+export async function removeBookmark(resource, data) {
+  return await requestData(resource, data);
+}
+
+export async function login(resource, data) {
+  return await requestData(resource, data);
+}
+
+export async function signup(resource, data) {
+  return await requestData(resource, data);
+}
