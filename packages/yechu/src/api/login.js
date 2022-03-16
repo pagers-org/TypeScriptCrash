d0374b885d50531bda5173d5335993664f4d6718
yechu <yechukim217@gmail.com>
2022-03-16 19:29:17 +0900

    remove useless

--- packages/yechu/src/api/login.js
+++
@@ -1,9 +0,0 @@
-import { parseData } from "./common";
-
-export async function login(url, data) {
-  return await parseData(url, data);
-}
-
-export async function signup(url, data) {
-  return await parseData(url, data);
-}
