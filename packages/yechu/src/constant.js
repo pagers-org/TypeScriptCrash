0d8e212c60c161b596668accf455d0bef41c4719
yechu <yechukim217@gmail.com>
2022-03-16 22:04:14 +0900

    feat: 상수 추가

--- packages/yechu/src/constant.js
+++ packages/yechu/src/constant.js
@@ -2,3 +2,5 @@ export const REG_EMAIL =
   /^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
 export const USER_TOKEN = 'user_token';
+export const LOGIN = '로그인';
+export const SIGN_UP = '회원가입';
