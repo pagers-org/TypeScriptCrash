2dde41228c9b911a6d807aff542cdf1dbcd87e07
wooooooood <13pft13@gmail.com>
2022-03-19 00:57:44 +0900

    fix. 변수 이름 반대로적음

--- packages/namu/src/constant/url.js
+++ packages/namu/src/constant/url.js
@@ -1,4 +1,4 @@
-export const BASE_CLIENT_URL = "http://localhost:3000";
-export const BASE_SERVER_URL = "http://localhost:5510";
+export const BASE_SERVER_URL = "http://localhost:3000";
+export const BASE_CLIENT_URL = "http://localhost:5510";
 
 export const FOX_URL = "https://randomfox.ca";
