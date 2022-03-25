aca296e0ca26735a51157ca52cf774d7c754878e
설아아빠 <dhrod0325@naver.com>
2022-03-25 21:11:18 +0900

    Apply suggestions from code review
    Co-authored-by: Areum Yang <areumsheep@gmail.com>

--- homework/dhrod0325/[2_1]_Calculate.ts
+++ homework/dhrod0325/[2_1]_Calculate.ts
@@ -132,7 +132,7 @@ function calculate(
   return calculator.calc(num1, num2, ...numbers);
 }
 
-console.log(calculate('+', 1, 3)); // 4
+console.log(calculate('add', 1, 3)); // 4
 console.log(calculate('sub', '3', 2)); // 1
 console.log(calculate('mul', 6, '9')); // 54
 console.log(calculate('div', '5', '4')); // 1
