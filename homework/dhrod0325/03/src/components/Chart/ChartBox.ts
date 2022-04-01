0dcd07163db690168a8a191aed46d06e04eb2dc0
설아아빠 <dhrod0325@naver.com>
2022-04-01 21:36:22 +0900

    🗃Refactor: Chart 옵션 분리

--- homework/dhrod0325/03/src/components/Chart/ChartBox.ts
+++ homework/dhrod0325/03/src/components/Chart/ChartBox.ts
@@ -1,24 +1,26 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
 import { Country } from 'covid';
-import { $, getDateString } from '@/lib/utils';
+import { $ } from '@/lib/utils';
 import { api } from '@/lib/Api';
 import { AsyncComponent } from '@/lib/Component';
 import { Chart } from 'chart.js';
+import { createChartOption } from '@/components/Chart/ChartOption';
+
+Chart.defaults.global.defaultFontColor = '#f5eaea';
+Chart.defaults.global.defaultFontFamily = 'Exo 2';
 
 export class ChartBox extends AsyncComponent {
   private readonly VIEW_DATE_COUNT = -14;
 
   private chart?: Chart;
 
-  private $container: HTMLCanvasElement;
+  public $container: HTMLCanvasElement;
+
   private countries: Country[] = [];
 
   constructor() {
     super();
-
-    Chart.defaults.global.defaultFontColor = '#f5eaea';
-    Chart.defaults.global.defaultFontFamily = 'Exo 2';
 
     this.$container = $('#lineChart') as HTMLCanvasElement;
   }
@@ -36,34 +38,6 @@ export class ChartBox extends AsyncComponent {
     this.countries = [...countries].slice(this.VIEW_DATE_COUNT);
   }
 
-  private createChartData(): number[] {
-    return this.countries.map(value => +value.Cases);
-  }
-
-  private createChartLabel(): string[] {
-    return this.countries.map(value => getDateString(value.Date).slice(5, -1));
-  }
-
-  private createChartOption(): Chart.ChartConfiguration {
-    const data = this.createChartData();
-    const labels = this.createChartLabel();
-
-    return {
-      type: 'line',
-      data: {
-        labels,
-        datasets: [
-          {
-            label: 'Confirmed for the last two weeks',
-            backgroundColor: '#feb72b',
-            borderColor: '#feb72b',
-            data,
-          },
-        ],
-      },
-    };
-  }
-
   private destroy() {
     this.chart && this.chart.destroy();
   }
@@ -73,7 +47,7 @@ export class ChartBox extends AsyncComponent {
 
     if (!ctx) throw new Error('chart create fail');
 
-    const option = this.createChartOption();
+    const option = createChartOption(this.countries);
 
     this.chart = new Chart(ctx, option);
   }
