9eb405a93f3c9d766c2bd2ac97001d94a9b97909
parksil0 <parksil00@naver.com>
2022-04-13 23:55:26 +0900

    feat : initialize next app

--- packages/seal/src/helper/dom.js
+++
@@ -1,8 +0,0 @@
-export const $ = selector => document.querySelector(selector);
-
-export const $all = selector => document.querySelectorAll(selector);
-
-export const toggleLoading = () => $('.loading').classList.toggle('hidden');
-
-export const setDisplay = (element, display) =>
-  (element.style.display = display);
