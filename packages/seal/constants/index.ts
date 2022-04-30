da48f158020b15400796f2cdb356248f420d1eb4
parksil0 <parksil00@naver.com>
2022-04-30 20:54:00 +0900

    feat : 상수 설정

---
+++ packages/seal/constants/index.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = 'http://localhost:3000';
+export const RANDOM_FOX_URL = 'https://randomfox.ca/images';
+
+export const FOX_PICTURE_MAX_NUMBER = 123;
+
+export const PAGE_SIZE = 10;
