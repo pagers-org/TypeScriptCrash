18e6d8a8eea3156fd50673642c773ea893e3a61d
parksil0 <parksil00@naver.com>
2022-04-30 19:10:04 +0900

    feat : image domain 설정

--- packages/seal/next.config.js
+++ packages/seal/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ['randomfox.ca'],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
